04b324ae74ce974f86f3e41bc6c341b317031790
Conner Anderson <cnnranderson@gmail.com>
2021-06-28 23:44:23 -0700

    This grid approach sucks

--- entities/grid/Grid.gd
+++ entities/grid/Grid.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 export(Vector2) var grid_size = Vector2(16, 16)
-export(int) var tile_size = 20
+export(int) var tile_size = 16
 
 var pos = Vector2(0, 0)
 
@@ -16,11 +16,20 @@ func _draw():
 	var grid_width = grid_size.x * tile_size
 	var grid_height = grid_size.y * tile_size
 	draw_rect(Rect2(-grid_width / 2, -grid_height / 2, grid_width, grid_height), Color.black)
-	draw_rect(Rect2(pos.x * tile_size, pos.y * tile_size, pos.x, pos.y), Color.white)
+	draw_rect(Rect2(pos.x * tile_size, pos.y * tile_size, tile_size, tile_size), Color.white)
 
 func _on_Grid_input_event(viewport, event, shape_idx):
+	if event is InputEventMouse:
+		pos = ((event.position - position) / tile_size).floor()
 	if event is InputEventMouseButton:
-		pos = ((event.position - position)).floor() / tile_size + grid_size / 2
 		if event.button_index == BUTTON_LEFT and event.pressed:
-			if GameState.selected_ship or true:
-				print(((event.position - position) / tile_size).floor() + grid_size / 2)
+			if GameState.selected_ship:
+				var loc = Vector2(position.x + pos.x * tile_size + tile_size / 2, position.y + pos.y * tile_size + tile_size / 2)
+				var normal = _grid_normalize(pos)
+				GameState.selected_ship.place(loc, normal)
+
+func _grid_to_world(coord: Vector2):
+	return position + pos * tile_size
+
+func _grid_normalize(coord: Vector2):
+	return coord + grid_size / 2
--- entities/ship/Ship.gd
+++ entities/ship/Ship.gd
@@ -1,10 +1,17 @@
 extends Node2D
 class_name Ship
 
+enum Orientation {VERTICAL, HORIZONTAL}
+
 export(Vector2) var grid_pos = Vector2.ZERO
 export(int, 0, 5) var hp = 5
 
+onready var start_pos = position
+onready var placed_pos = position
+
 var selected = false
+var placed = false
+var orientation = Orientation.HORIZONTAL
 
 func _ready():
 	pass
@@ -12,6 +19,29 @@ func _ready():
 func _process(delta):
 	if selected:
 		position = lerp(position, get_global_mouse_position(), 0.3)
+	elif not selected and not placed:
+		position = lerp(position, start_pos, 0.1)
+	elif placed:
+		position = lerp(position, placed_pos, 0.1)
+
+func place(location: Vector2, grid_pos: Vector2):
+	placed_pos = location
+	placed = true
+	match orientation:
+		Orientation.HORIZONTAL:
+			var diff = Vector2(0, 0)
+			if grid_pos.x < 2:
+				diff.x = (int(grid_pos.x) % 2 - int(hp / 2.0)) * 16
+			elif grid_pos.x > 13:
+				diff.x = -(15 - grid_pos.x - int(hp / 2.0)) * 16
+			placed_pos -= diff
+		Orientation.VERTICAL:
+			var diff = Vector2(0, 0)
+			if grid_pos.y < 2:
+				diff.y = (int(grid_pos.y) % 2 - int(hp / 2.0)) * 16
+			elif grid_pos.y > 13:
+				diff.y = -(15 - grid_pos.y - int(hp / 2.0)) * 16
+			placed_pos -= diff
 
 func _on_Area2D_mouse_entered():
 	material.set_shader_param("intensity", 400)
@@ -21,11 +51,18 @@ func _on_Area2D_mouse_exited():
 	material.set_shader_param("intensity", 0)
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
-		if event.button_index == BUTTON_LEFT:
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
 			selected = !selected
+			if selected:
+				placed = false
+				modulate.a = 0.9
+			else:
+				modulate.a = 1.0
+		elif event.button_index == BUTTON_LEFT and not event.pressed:
 			GameState.selected_ship = self if selected else null
 		
-		if event.button_index == BUTTON_RIGHT:
+		if event.button_index == BUTTON_RIGHT and event.pressed:
 			if selected:
+				orientation = Orientation.HORIZONTAL if orientation == Orientation.VERTICAL else Orientation.VERTICAL
 				rotate(PI / 2)

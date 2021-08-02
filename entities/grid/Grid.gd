58e66422fe323f67211c8303aa0dc9c359e240ca
Conner Anderson <cnnranderson@gmail.com>
2021-08-02 00:09:26 -0700

    Almost have grid placement to a better state

--- entities/grid/Grid.gd
+++ entities/grid/Grid.gd
@@ -1,20 +1,23 @@
 tool
-extends Area2D
+extends Node2D
 class_name ShipGrid
-
-enum GridItem {NONE, SHIP, MISS}
 
 export var grid_size : Vector2 = Vector2(16, 16) setget set_grid_size
 export var tile_size : Vector2 = Vector2(16, 16) setget set_tile_size
 
+onready var area_shape = $Area2D/Shape
+
 var pos = Vector2(0, 0)
-var mouse_inside = false
+var hovering = false
 
 func _ready():
-	$Shape.shape.extents = grid_size / 2 * tile_size
+	set_meta("entity_type", "grid")
+	$Area2D.connect("mouse_entered", self, "_on_mouse_over", [true])
+	$Area2D.connect("mouse_exited", self, "_on_mouse_over", [false])
+	area_shape.shape.extents = grid_size / 2 * tile_size
 
 func _update_grid():
-	$Shape.shape.extents = grid_size / 2 * tile_size
+	area_shape.shape.extents = grid_size / 2 * tile_size
 	$Border.rect_position.x = -(grid_size.x / 2 * tile_size.x + 3)
 	$Border.rect_position.y = -(grid_size.y / 2 * tile_size.y + 3)
 	$Border.rect_size.x = grid_size.x * tile_size.x + 6
@@ -28,7 +31,7 @@ func _update_grid():
 		for i in range(grid_size.x):
 			GameState.grid.append([])
 			for j in range(grid_size.y):
-				GameState.grid[i].append(GridItem.NONE)
+				GameState.grid[i].append(0)
 
 func set_grid_size(value):
 	grid_size = value
@@ -44,25 +47,29 @@ func _grid_to_world(coord: Vector2):
 func _grid_normalize(coord: Vector2):
 	return coord + (grid_size / 2)
 
+func _scale(value):
+	return value * tile_size.x
+
 func _process(delta):
 	update()
 
 func _draw():
-	if mouse_inside:
-		draw_rect(Rect2(pos.x * tile_size.x, pos.y * tile_size.y, tile_size.x, tile_size.y), Color.black)
+	if hovering and GameState.selected_ship:
+		var ship_hp = GameState.selected_ship.hp
+		if GameState.selected_ship.orientation == 1:
+			draw_rect(Rect2(_scale(max(-8, min(pos.x - ship_hp / 2, 8 - ship_hp))), _scale(pos.y), _scale(ship_hp), tile_size.y), Color.black)
+		else:
+			draw_rect(Rect2(_scale(pos.x), _scale(max(-8, min(pos.y - ship_hp / 2, 8 - ship_hp))), tile_size.x, _scale(ship_hp)), Color.black)
 
-func _on_Grid_input_event(viewport, event, shape_idx):
+func place_ship(ship):
+	print("Placing")
+	var loc = _grid_to_world(pos)
+	var normal = _grid_normalize(pos)
+	GameState.selected_ship.place(self, loc, normal)
+
+func _on_mouse_over(over):
+	hovering = over
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouse:
 		pos = ((event.position - position) / tile_size).floor()
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT and event.pressed:
-			if GameState.selected_ship:
-				var loc = _grid_to_world(pos)
-				var normal = _grid_normalize(pos)
-				GameState.selected_ship.place(self, loc, normal)
-
-func _on_Grid_mouse_entered():
-	mouse_inside = true
-
-func _on_Grid_mouse_exited():
-	mouse_inside = false
--- entities/ship/Ship.gd
+++ entities/ship/Ship.gd
@@ -6,7 +6,7 @@ enum Orientation {VERTICAL, HORIZONTAL}
 export(int, 0, 5) var hp = 5
 
 onready var start_pos = position
-onready var placed_pos = position
+onready var target_pos = position
 
 var dragging = false
 var placed = false
@@ -14,22 +14,20 @@ var orientation = Orientation.HORIZONTAL
 var rot = rotation
 
 func _ready():
-	pass
+	set_meta("entity_type", "ship")
 
 func _process(delta):
 	if dragging:
-		var target = _adjust_position(get_viewport().get_mouse_position())
-		position = lerp(position, target, 0.5)
-	elif not dragging and not placed:
-		position = lerp(position, start_pos, 0.1)
-	elif placed:
-		position = lerp(position, placed_pos, 0.1)
+		target_pos = _adjust_position(get_viewport().get_mouse_position())
 	
+	position = lerp(position, target_pos, 0.4)
 	rotation = lerp(rotation, rot, 0.5)
 
 func place(grid: ShipGrid, location: Vector2, grid_pos: Vector2):
-	placed_pos = location
+	z_index = 0
+	modulate.a = 1.0
 	placed = true
+	target_pos = location
 	var diff = Vector2(0, 0)
 	match orientation:
 		Orientation.HORIZONTAL:
@@ -44,7 +42,8 @@ func place(grid: ShipGrid, location: Vector2, grid_pos: Vector2):
 				diff.y = min((grid.grid_size.y - grid_pos.y - int((hp + 1) / 2.0)) * 16, 0)
 				
 	diff = _adjust_position(diff)
-	placed_pos += diff
+	target_pos += diff
+	GameState.selected_ship = null
 
 func _adjust_position(target: Vector2):
 	if hp % 2 == 0:
@@ -56,7 +55,8 @@ func _adjust_position(target: Vector2):
 	return target
 
 func _on_Area2D_mouse_entered():
-	material.set_shader_param("intensity", 400)
+	if GameState.selected_ship == null or GameState.selected_ship == self:
+		material.set_shader_param("intensity", 400)
 
 func _on_Area2D_mouse_exited():
 	if dragging: return
@@ -65,15 +65,38 @@ func _on_Area2D_mouse_exited():
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
+			# Ignore input if a different ship is already being dragged around
+			if GameState.selected_ship != self and GameState.selected_ship != null:
+				return
+				
+			# Enable/disable dragging
 			dragging = !dragging
+			
 			if dragging:
-				placed = false
-				placed_pos = start_pos
 				modulate.a = 0.9
+				z_index = 1
+				placed = false
+				GameState.selected_ship = self
 			else:
-				modulate.a = 1.0
-		elif event.button_index == BUTTON_LEFT and not event.pressed:
-			GameState.selected_ship = self if dragging else null
+				var grid_overlap = null
+				var ship_overlap = null
+				for area in $Area2D.get_overlapping_areas():
+					match area.get_parent().get_meta("entity_type"):
+						"ship":
+							print("Found ship")
+							ship_overlap = area.get_parent() as Ship
+						"grid":
+							print("Found grid")
+							grid_overlap = area.get_parent() as ShipGrid
+				
+				if ship_overlap:
+					dragging = true
+				elif grid_overlap:
+					grid_overlap.place_ship(self)
+				else:
+					z_index = 0
+					target_pos = start_pos
+					GameState.selected_ship = null
 		
 		if event.button_index == BUTTON_RIGHT and event.pressed:
 			if dragging:
